36b59bb60a11b0d83484e59c3665c21eae065597
yuusunn <hyusynn@gmail.com>
2025-08-11 16:46:29 +0900

    [fix]devモードでもS3からデータ取得する

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // devモードでもS3から練習問題を取得する
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://aspsz9mjw3.execute-api.ap-northeast-1.amazonaws.com",
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api/, "/prod"),
+      },
+    },
+  },
 })
